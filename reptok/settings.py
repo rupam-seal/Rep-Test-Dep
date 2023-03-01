e64aac32d4f9dfec1a01746e1d513c23aa981833
rupam-seal <rupam.x.seal@gmail.com>
2023-03-01 19:54:42 +0530

    modified

--- reptok/settings.py
+++ reptok/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '*']
 
 
 # Application definition
