47d388e5aa52d839113e465a9dc905b4da4d32cd
rupam-seal <rupam.x.seal@gmail.com>
2023-03-01 21:26:33 +0530

    modified

--- reptok/settings.py
+++ reptok/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -46,7 +51,8 @@ INSTALLED_APPS = [
     'apps.orders.apps.OrdersConfig',
     'apps.staff.apps.StaffConfig',
     'apps.core.apps.CoreConfig',
-    'apps.staffDashboard.apps.StaffdashboardConfig'
+    'apps.staffDashboard.apps.StaffdashboardConfig',
+    'environ'
 ]
 
 MIDDLEWARE = [
@@ -87,8 +93,12 @@ WSGI_APPLICATION = 'reptok.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': env("DATABASE_HOST"),
+        'PORT': env("DATABASE_PORT"),
     }
 }
 
