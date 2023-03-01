34caba54fc9d2f0e9a4c66c5eaa81a0c247a190d
rupam-seal <rupam.x.seal@gmail.com>
2023-03-01 19:40:54 +0530

    modified

--- staticfiles_build/static/js/script.js
+++
@@ -1,9 +0,0 @@
-// adding href to the table raw
-document.addEventListener("DOMContentLoaded",() =>{
-    const rows = document.querySelectorAll("tr[data-href]");
-    rows.forEach(row => {
-        row.addEventListener("click", () => {
-            window.location.href = row.dataset.href;
-        });
-    });
-});
